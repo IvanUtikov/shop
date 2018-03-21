f808d07ef82f9e29f1965877d44fbc33d85f195b
Ivan <ivan@MacBook-Pro-Ivan.local>
2018-03-21 15:17:13 +0200

    Add clean layout

---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>!!!"			
+end
+
+get '/aboutus' do
+	erb :about
+end
+
+get '/visit' do
+	erb :visit
+end
+
+get '/contact' do
+erb :contact
+end
